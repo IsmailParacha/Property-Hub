769724732c2d8494b91096896ac3a062647b894c
IsmailParacha <ismailjanparacha@gmail.com>
2022-08-04 01:26:28 +0500

    commit

--- lib/Seller/NavigationDrawer.dart
+++ lib/Seller/NavigationDrawer.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:share/share.dart';
 
 class NavigationDrawer extends StatefulWidget {
