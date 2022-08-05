1d624cb5339c35679c5f014460268bba22d59b50
IsmailParacha <ismailjanparacha@gmail.com>
2022-08-05 17:40:26 +0500

    Saved Searches page

--- lib/Screen/SavedScrren.dart
+++ lib/Screen/SavedScrren.dart
@@ -17,10 +17,12 @@ class SavedScreen extends StatelessWidget {
         padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
         child: ListView(
           children: [
+            const SizedBox(height: 15),
             Text(
               "Saved Searches",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
             ),
+            const SizedBox(height: 20),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -30,22 +32,35 @@ class SavedScreen extends StatelessWidget {
                       'https://assets4.lottiefiles.com/packages/lf20_xbf1be8x.json'),
                   Text(
                     "No Saved Searches!",
-                    style: TextStyle(fontSize: 20),
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Color.fromRGBO(57, 65, 96, 1.0),
+                        fontWeight: FontWeight.bold),
                   ),
-                  Text(""),
-                  Expanded(
-                      child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(57, 65, 96, 1.0),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Center(
-                            child: const Text(
-                              'Submit Button',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white),
-                            ),
-                          ))),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      "Looks Like you haven't search any ads.\nYou can search add by tapping on search\nbutton & also from search icons & Search bar in home page.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(57, 65, 96, 1.0),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(
+                          child: const Text(
+                            'Submit Button',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        )),
+                  )
                 ],
               ),
             )
