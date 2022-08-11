6318300df6773c1751d2c6cb26e037ba7c65aa90
IsmailParacha <ismailjanparacha@gmail.com>
2022-08-11 18:06:19 +0500

    commit

--- lib/Screen/Utils.dart
+++ lib/Screen/Utils.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+// ignore: import_of_legacy_library_into_null_safe
+import '../main.dart';
+
+class Utils {
+  static showSnackBar(String? text) {
+    if (text == null) return;
+    final snackBar = SnackBar(
+        content: Text('$text'),
+        backgroundColor: Color.fromRGBO(255, 182, 0, 1.0));
+    messengerKey.currentState!
+      ..removeCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+}
