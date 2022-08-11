d829a6365015dcf79abf2622f6a623e55f4b4abd
IsmailParacha <ismailjanparacha@gmail.com>
2022-08-11 18:07:33 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
+// @dart=2.9
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:property/Dashboard/Dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDR6I9UKhN7n0z3jtTsUrCoGhXf6klfc60", // Your apiKey
+      appId: "1:994634378279:android:e95b37bcf91efd1a557202", // Your appId
+      messagingSenderId: "XXX", // Your messagingSenderId
+      projectId: "jhjhj-99c05", // Your projectId
+    ),
+  );
+  runApp(MyApp());
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+final messengerKey = GlobalKey<ScaffoldMessengerState>();
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scaffoldMessengerKey: messengerKey,
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        // body: Property(),
+        body: Center(
+            child: AnimatedSplashScreen(
+                duration: 2500,
+                splashTransition: SplashTransition.slideTransition,
+                splashIconSize: 400,
+                splash: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage('images/logo3.jpeg'),
+                  )),
+                ),
+                nextScreen: DashBoard()
+                // nextScreen: Home(),
+                )),
+      ),
+    );
+  }
+}
